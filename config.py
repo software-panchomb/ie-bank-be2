4cf0b175644f9035880b631a21b329c2bbe735dc
sarwari-mallela <123460981+sarwari-mallela@users.noreply.github.com>
2023-10-09 15:56:17 +0200

    Created a class in config.py for UAT environment

--- config.py
+++ config.py
@@ -26,3 +26,11 @@ class DevelopmentConfig(Config):
     DEBUG = True
 
 # Create class for UAT
+class UATConfig(Config): 
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    dbuser=os.getenv('DBUSER'),
+    dbpass=os.getenv('DBPASS'),
+    dbhost=os.getenv('DBHOST'),
+    dbname=os.getenv('DBNAME')
+    )
+    DEBUG = True
