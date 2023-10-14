1ec4cb6e548dff3dd6330f2740c2c0072b50c862
sarwari-mallela <123460981+sarwari-mallela@users.noreply.github.com>
2023-10-14 18:23:17 +0200

    Changes in __init__.py file

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -18,6 +18,9 @@ elif os.getenv('ENV') == 'dev':
 elif os.getenv('ENV') == 'ghci':
     print("Running in github mode")
     app.config.from_object('config.GithubCIConfig')
+elif os.getenv("ENV") == "uat":
+    print("Running in UAT mode")
+    app.config.from_object("config.UATConfig")
 else:
     print("Running in production mode")
     app.config.from_object('config.ProductionConfig')
